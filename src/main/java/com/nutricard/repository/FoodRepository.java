cd676f45ccf021336553b4afe22ed4622f8457fc
Kwasi Asare <e.asare789@hotmail.com>
2026-05-31 18:14:14 +0100

    feat: add GET /foods?search= endpoint

--- src/main/java/com/nutricard/repository/FoodRepository.java
+++ src/main/java/com/nutricard/repository/FoodRepository.java
@@ -4,6 +4,9 @@ import com.nutricard.model.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
+    List<Food> findByNameContainingIgnoreCase(String name);
 }
